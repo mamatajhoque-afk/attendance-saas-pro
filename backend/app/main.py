8976387ac3cacc2cc3ffc390aabe021b550299a9
mamatajhoque-afk <mamatajhoque@gmail.com>
2026-03-08 08:55:55 +0600

    Update CORS origins for frontend access

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 origins = [
     "http://localhost:5173",
     "http://localhost:3000",
-    "https://attendance-frontends.onrender.com"
+    "https://attendence-saas.onrender.com"
 ]
 
 app.add_middleware(
